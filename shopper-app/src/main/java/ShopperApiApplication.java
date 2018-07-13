5a63b9ef212dcbb00edcfd75e0bd6b75d69af062
Jakub Romaniszyn <jakub.romaniszyn@xe05.ey.com>
2018-07-13 14:22:19 +0200

    SHOP-1: Add modules

---
+++ shopper-app/src/main/java/ShopperApiApplication.java
@@ -0,0 +1,12 @@
+package com.qprogramming.shopper.shopper.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopperApiApplication.class, args);
+	}
+}
