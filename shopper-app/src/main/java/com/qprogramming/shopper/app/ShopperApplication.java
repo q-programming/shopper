5d32ebd0140b0e5303fa1a3b25e99e7f6da18f46
Jakub Romaniszyn <jakub.romaniszyn@xe05.ey.com>
2018-07-16 13:51:06 +0200

    SHOP-1: Built executable war

--- shopper-app/src/main/java/com/qprogramming/shopper/app/ShopperApplication.java
+++ shopper-app/src/main/java/com/qprogramming/shopper/app/ShopperApplication.java
@@ -2,11 +2,18 @@ package com.qprogramming.shopper.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShopperApplication {
+public class ShopperApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopperApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShopperApplication.class);
+    }
 }
