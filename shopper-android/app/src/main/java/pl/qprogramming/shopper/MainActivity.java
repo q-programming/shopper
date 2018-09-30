328064915a3da35ded3da5313c35d1ada3c8f889
Jakub Romaniszyn <khobar@interia.pl>
2018-09-30 12:53:17 +0200

    SHOP-80: Create simple android webview application to load page

---
+++ shopper-android/app/src/main/java/pl/qprogramming/shopper/MainActivity.java
@@ -0,0 +1,63 @@
+package pl.qprogramming.shopper;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        imageView = findViewById(R.id.splashImage);
+        webView.setWebViewClient(new CustomWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36");
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            cookieManager.setAcceptThirdPartyCookies(webView, true);
+        }
+        webView.loadUrl("https://q-programming.pl/shopper");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // This allows for a splash screen
+// (and hide elements once the page loads)
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+            webview.setVisibility(webview.INVISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            imageView.setVisibility(View.GONE);
+            view.setVisibility(webView.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
+    }
+}
