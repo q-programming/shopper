2a5372c707c778ccd70891f268610df667a255f5
Jakub Romaniszyn <jakub.romaniszyn@gmail.com>
2018-10-01 14:04:24 +0200

    SHOP-77 Highlighter pipe is no longer removing spaces

--- shopper-web/src/app/directives/highlight.directive.ts
+++ shopper-web/src/app/directives/highlight.directive.ts
@@ -1,16 +1,21 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Pipe({name: 'highlight'})
 export class HighlightDirective implements PipeTransform {
 
-    transform(text: string, search): string {
+    constructor(public sanitizer: DomSanitizer) {
+    }
+
+    transform(text: string, search): SafeHtml {
         if (search && typeof search === 'string') {
             let pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
             pattern = pattern.split(' ').filter((t) => {
                 return t.length > 0;
             }).join('|');
+            text = text.replace(/\ /g, '&nbsp;');
             let regex = new RegExp(pattern, 'gi');
-            return search ? text.replace(regex, (match) => `<b>${match}</b>`) : text;
+            return this.sanitizer.bypassSecurityTrustHtml(search ? text.replace(regex, (match) => `<b>${match}</b>`) : text);
         }
         return text;
     }
